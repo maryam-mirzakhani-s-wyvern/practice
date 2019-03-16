0e406f6dd60a426e506c27a2ab07c452cc37ea02
Torri Raines <raines.torri@gmail.com>
2019-03-16 14:00:45 -0400

    helper functions and rank order output

--- experiment2.js
+++ experiment2.js
@@ -87,18 +87,69 @@ const guessMore = [
   {sleep: 0.33, sun: 0.25, exercise: 1, social: 1, relax: 0, meals: .5, work: 0.4} //pleasant: .3-.6, tension: .6-.8, energy: .3-.5
 ]
 
-const r1 = moodNetwork.run(guessMore[0])
-const r2 = moodNetwork.run(guessMore[1])
-const r3 = moodNetwork.run(guessMore[2])
-const r4 = moodNetwork.run(guessMore[3])
-const r5 = moodNetwork.run(guessMore[4])
-const r6 = moodNetwork.run(guessMore[5])
+function avgOfRuns(input, network, times){
+    let results = []
+    for (let i = 0; i < times; i++){
+        results.push(network.run(input))
+    }
+    let o1 = 0
+    let o2 = 0
+    let o3 = 0
+    for (let j = 0; j < times; j++){
+        o1 += results[j].pleasant
+        o2 += results[j].tension
+        o3 += results[j].energy
+    }
+    return {pleasant: o1 / times, tension: o2 / times, energy: o3 / times}
+}
+
+function orderResults(resultObj){
+    let order = []
+    for (let key in resultObj){
+        if (Object.prototype.hasOwnProperty.call(resultObj, key)){
+            order.push([key, resultObj[key]])
+        }
+    }
+    order.sort(function(a, b) {
+        var valueA, valueB;
+    
+        valueA = a[1];
+        valueB = b[1];
+        if (valueA < valueB) {
+            return -1;
+        }
+        else if (valueA > valueB) {
+            return 1;
+        }
+        return 0;
+    });
+    return order
+}
+// const r1 = moodNetwork.run(guessMore[0])
+// const r2 = moodNetwork.run(guessMore[1])
+// const r3 = moodNetwork.run(guessMore[2])
+// const r4 = moodNetwork.run(guessMore[3])
+// const r5 = moodNetwork.run(guessMore[4])
+// const r6 = moodNetwork.run(guessMore[5])
+
+const r1 = avgOfRuns(guessMore[0], moodNetwork, 100)
+const r2 = avgOfRuns(guessMore[1], moodNetwork, 100)
+const r3 = avgOfRuns(guessMore[2], moodNetwork, 100)
+const r4 = avgOfRuns(guessMore[3], moodNetwork, 100)
+const r5 = avgOfRuns(guessMore[4], moodNetwork, 100)
+const r6 = avgOfRuns(guessMore[5], moodNetwork, 100)
 
 console.log("Result1: \n", r1)
+console.log("order: ", orderResults(r1))
 console.log("Result2: \n", r2)
+console.log("order: ", orderResults(r2))
 console.log("Result3: \n", r3)
+console.log("order: ", orderResults(r3))
 console.log("Result4: \n", r4)
+console.log("order: ", orderResults(r4))
 console.log("Result5: \n", r5)
+console.log("order: ", orderResults(r5))
 console.log("Result6: \n", r6)
+console.log("order: ", orderResults(r6))
 
 //console.log(moodNetwork.toJSON())
